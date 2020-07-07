998ced50e5024ecef9e7c408227b708cf7e39384
awolfsdorf <alexiswolfsdorf@gmail.com>
2020-07-07 00:30:38 -0300

    Segment.py

---
+++ metnum-tp3-2020-1c/notebooks/Segment.py
@@ -0,0 +1,37 @@
+import numpy as np
+from sklearn.model_selection import KFold
+
+
+class Segment:
+    def init(self, name, linear_regressor, features):
+        self.name = name
+        self.linear_regressor = linear_regressor
+        self.features = features
+
+    def fit(self, A, reales):
+        self.linear_regressor.fit(A, reales)
+
+    def predict(self, A):
+        return self.linear_regressor.predict(A)
+
+    def execute(self, A, reales, metrics, k=5):
+
+        self.errors = np.zeros_like(metrics)
+
+        kf = KFold(n_splits=k)
+        for train_index, test_index in kf.split(A):
+            # Divido en datos de entrenamiento y testeo
+            A_train, A_test = A[train_index], A[test_index]
+            reales_train, reales_test = reales[train_index], reales[test_index]
+            # Fiteo con los de entrenamiento
+            self.fit(A_train, reales_train)
+
+            # Predigo con los de testeo
+            aproximados = self.predict(A_test)
+            i = 0
+            # Calculo y guardo las metricas pasadas por parametro
+            for metric in metrics:
+                self.errors[i] += metric(reales_test, aproximados)
+                i += 1
+
+        self.errors /= k
