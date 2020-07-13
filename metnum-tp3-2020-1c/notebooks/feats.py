85e30d242045eae8990b363d5b0c5157fec43fb0
ToOrtner <tortner@dc.uba.ar>
2020-07-12 21:23:13 -0300

    agrego feature engineer, falta aplicarlo

---
+++ metnum-tp3-2020-1c/notebooks/feats.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+
+#esta lista está en orden, asi que su peso es aun mayor segun su posicion
+
+ciudades = np.array(["San Pedro Garza García", "Colima", "Mérida", "San Nicolás de los Garza", "Saltillo", "Mazatlán", "Apodaca", "Chihuahua",
+                     "Aguascalientes", "Mexicali", "Querétaro", "Campeche", "Guadalupe", "Matamoros", "Nuevo Laredo", "Venustiano Carranza", "Torreón",
+                     "León", "Culiacán", "Hermosillo", "Monterrey", "Reynosa", "Benito Juárez", "Zapopan", "Zacatecas", "La Paz", "Manzanillo",
+                     "Iztapalapa", "Gómez Palacio", "Guanajuato", "Cuajimalpa de Morelos", "Veracruz", "Lázaro Cárdenas", "Miguel Hidalgo",
+                     "Guadalajara", "Azcapotzalco", "Tlaquepaque", "Carmen", "Cuauhtémoc", "Gustavo A. Madero", "Tepic", "Coyoacán",
+                     "Iztacalco", "Benito Juárez", "Pachuca", "Milpa Alta", "Durango", "Juárez", "Tlaxcala", "Toluca", "Tlalpan", "Alvaro Obregón",
+                     "Chimalhuacán", "Nezahualcóyotl", "Tlalnepantla de Baz", "Chilpancingo de los Bravo", "Xalapa", "Cajeme", "Acapulco de Juárez", "Xochimilco",
+                     "San Luis Potosí", "La Magdalena Contreras", "Villahermosa", "Tijuana", "Morelia", "Oaxaca de Juárez", "Cuernavaca", "Tapachula",
+                     "Naucalpan de Juárez", "Tláhuac", "Tuxtla Gutiérrez", "Puebla", "Victoria", "Chetumal", "Tehuacán", "Ecatepec de Morelos"])
+
+
+def newfeats(df):
+
+    #valor predeterminado para las ciudades que no esten en este ranking
+    df['mejorciudad'] = 1
+    #valor que llevará la ciudad según su posición en el ranking
+    posicion = 1
+    for c in ciudades:
+        df.loc[df['ciudad'] == c, 'mejorciudad'] = 100 / posicion
+        posicion += 1
+
+
+    #tropico de cancer = a latitud  23,43
+    df = df.dropna(subset=['lat', 'piscina'])
+
+    df['calurosa'] = False
+    df.loc[((df['lat'] < 23.43)) & ((df['piscina'] > 0)) , 'calurosa'] = True
+
+    #escuelas
+    df = df.dropna(subset=['habitaciones', 'escuelascercanas'])
+
+    df['parachicos'] = False
+    df.loc[((df['habitaciones'] > 2)) & ((df['escuelascercanas'] > 0)) , 'parachicos'] = True
+
+    return df
+
